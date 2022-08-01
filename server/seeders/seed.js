4bc1a8872c807c34497c65682f9cef9fadb05185
Sela Privette <selaprivette@yahoo.com>
2022-08-01 13:29:25 -0400

    update to group seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -2,15 +2,18 @@ const db = require('../config/connection');
 const { Ticket, User, Group, Project } = require('../models');
 const ticketSeeds = require('./ticketsSeeds.json');
 const userSeeds = require('./userSeeds.json');
-
 db.once('open', async () => {
   try {
     await Ticket.deleteMany({});
     await Ticket.create(ticketSeeds);
     await User.deleteMany({});
-    await User.create(userSeeds);
+    const users = await User.create(userSeeds);
     await Group.deleteMany({});
-    await Group.create(groupSeeds);
+    const group = await Group.create(groupSeeds);
+    for (user of users){
+	Group.users.push(user._id)
+    }
+    Group.save();
     await Project.deleteMany({});
     await Project.create(projectSeeds);
     console.log('all done!');
