73ebe3027cbf8f52e0923932568346484480b865
Sela Privette <selaprivette@yahoo.com>
2022-08-01 12:37:11 -0400

    user seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,12 +1,14 @@
 const db = require('../config/connection');
-const { Ticket } = require('../models');
+const { Ticket, User, Group, Project } = require('../models');
 const ticketSeeds = require('./ticketsSeeds.json');
+const userSeeds = require('./userSeeds.json');
 
 db.once('open', async () => {
   try {
     await Ticket.deleteMany({});
     await Ticket.create(ticketSeeds);
-
+    await User.deleteMany({});
+    await User.create(userSeeds);
     console.log('all done!');
     process.exit(0);
   } catch (err) {
