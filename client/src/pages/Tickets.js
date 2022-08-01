4144b33c46a2453700b1f6796af15de06eaf7e27
Sela Privette <selaprivette@yahoo.com>
2022-08-01 18:21:03 -0400

    ticket page

--- client/src/pages/Tickets.js
+++ client/src/pages/Tickets.js
@@ -1,2 +1,8 @@
 import React from "react";
-import { useQuery } from '@apollo/client';
+import { useQuery } from "@apollo/client";
+
+import TicketList from "../components/TicketList";
+import TicketsList from "../components/TicketsList";
+import TicketForm from "../components/TicketForm";
+
+import { QUERY_TICKETS, QUERY_SINGLE_TICKET } from '../utils/queries';
