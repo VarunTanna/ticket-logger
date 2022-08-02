219aa96c0af3305c5e1fa1f7b9ad68c45de65b3f
Sela Privette <selaprivette@yahoo.com>
2022-08-02 12:14:26 -0400

    queries

--- server/routes/api/project-routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { 
-  createProject
-} = require('../../controllers/project-controller');
-
-router.route('/')
-  .post(createProject);
-
-module.exports = router;
