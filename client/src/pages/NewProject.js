d40806cb20b97e0637b730e2c5f9e75e3dc4f72b
Chris Okamuro <cokamuro@gmail.com>
2022-08-04 11:11:45 -0400

    added repo checking to the createproject resolver

--- client/src/pages/NewProject.js
+++ client/src/pages/NewProject.js
@@ -5,60 +5,47 @@ import { CREATE_PROJECT } from '../utils/mutations'
 import { QUERY_GROUPS} from '../utils/queries'
 import Select from "react-select";
 import { useNavigate } from 'react-router-dom';
-
 function NewProject() {
-
   const { loading, data } = useQuery(QUERY_GROUPS)
-
   const navigate = useNavigate();
-
   const groupList = data?.groups || [];
-
   console.log(groupList);
-
   const groupOptions = [];
-
   for (let i=0; i < groupList.length; i++) {
-    let option = { key: groupList[i]._id, value: groupList[i]._id, label: groupList[i].name };
+    let option = { key: groupList[i]._id, value: groupList[i].users, label: groupList[i].name };
     groupOptions.push(option);
   };
-
   const [projectName, setProjectName] = useState('');
   const [projectRepo, setProjectRepo] = useState('');
   const [projectGroupId, setProjectGroupId] = useState();
-
   const [createProject, {error}] = useMutation(CREATE_PROJECT);
-
   const inputChangeA = (e) => {
     const { name, value } = e.target;
     setProjectName(value);
   };
-
   const inputChangeB = (e) => {
     const { name, value } = e.target;
     setProjectRepo(value);
   };
-
   const handleChange = (e) => {
     console.log(e);
-    setProjectGroupId(e.value);
+    setProjectGroupId(e.key);
   };
-
   const handleSubmit =  async (e) => {
     e.preventDefault();
     console.log(projectRepo);
+    console.log(projectGroupId);
     const { data } =  await createProject({
       variables: { groupId: projectGroupId, name: projectName, repo: projectRepo }
     });
     navigate('/project');
   }
-
   return (
       <>
           <h1>Create a Project</h1>
           <form className="form" style={style.form}>
               <label>Name:</label>
-              <input 
+              <input
                   value={projectName}
                   name="name"
                   type="text"
@@ -67,11 +54,11 @@ function NewProject() {
                   style={style.input}
               />
               <label>Repository:</label>
-              <textarea 
+              <input
                   rows={4}
                   value={projectRepo}
                   name="repo"
-                  type="textarea"
+                  type="text"
                   placeholder="enter github repo"
                   onChange={inputChangeB}
                   style={style.input}
@@ -92,6 +79,7 @@ function NewProject() {
                   Submit
               </button>
           </form>
+
           {error && (
               <div>
                   <p className="error-text">{error}</p>
@@ -100,7 +88,6 @@ function NewProject() {
       </>
   )
 }
-
 const style = {
   form: {
       margin: '18px',
@@ -111,5 +98,4 @@ const style = {
       width: '100%',
   },
 }
-
 export default NewProject;
